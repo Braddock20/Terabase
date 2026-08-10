54e4ce8d0d1ae8933c395b8b4a11bee161a2a173
Braddock20 <braddockikubano@gmail.com>
2026-08-10 14:04:06 +0300

    Initial commit

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,6 +1,8 @@
-import base64,os
-os.environ['FSE_MASTER_KEY']=base64.urlsafe_b64encode(os.urandom(32)).decode()
+import os
+os.environ['TERABOX_CLIENT_ID']='test-client'
+os.environ['TERABOX_CLIENT_SECRET']='test-secret'
+os.environ['TERABOX_PRIVATE_SECRET']='test-private'
 from app.crypto import Crypto
 
 def test_roundtrip():
- c=Crypto(os.environ['FSE_MASTER_KEY']); n,x=c.encrypt(b'hello',b'a'); assert c.decrypt(n,x,b'a')==b'hello'
+    c=Crypto(); n,x=c.encrypt(b'hello',b'a'); assert c.decrypt(n,x,b'a')==b'hello'
